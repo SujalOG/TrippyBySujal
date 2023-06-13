5a5f425ae03ee20b0b9df5e2baff599a511a7242
SujalOG <129844925+SujalOG@users.noreply.github.com>
2023-06-13 21:34:23 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Routes/Home";
 import About from "./Routes/About.js";
 import Service from "./Routes/Service.js";
 import Contact from "./Routes/Contact.js";
+import Signup from "./Routes/Sign-Up";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/service" element={<Service />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
