eb6f19bfead275f34598a82b85709d6dc50a2bbb
SujalOG <129844925+SujalOG@users.noreply.github.com>
2023-06-13 21:32:42 +0530

    Add files via upload

---
+++ src/Routes/Sign-Up.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+import SignUpPage from "../components/signup";
+
+const Signup = () => {
+  return (
+    <>
+        <Navbar />
+        <SignUpPage />
+        
+    </>
+  );
+};
+
+export default Signup;
